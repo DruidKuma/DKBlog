400203a9248abba14ff97a45a91f8e90785301c4
Iurii Miedviediev <druidkuma@gmail.com>
2016-08-15 16:41:01 +0300

    Implemented layout with some stubs for functionality for DB properties

--- src/main/resources/static/custom/property/systemPropertiesController.js
+++ src/main/resources/static/custom/property/systemPropertiesController.js
@@ -8,5 +8,22 @@ angular.module("blogApp")
             $scope.pageHeading.title = "System Properties";
         });
 
+        $scope.loadProperties = function() {
+            $scope.loadingProcess = true;
+            Property.all().then(function(response) {
+                $scope.allProperties = response.data;
+            }).finally(function() {
+                $scope.loadingProcess = false;
+            });
+        };
 
+        $scope.saveProperty = function(data, id) {
+           console.log(data);
+        };
+
+        $scope.removeProperty = function(property) {
+            console.log(property);
+        };
+
+        $scope.loadProperties();
     }]);
