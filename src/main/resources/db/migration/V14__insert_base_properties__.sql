199b65613831cf16d64493608bc42ce70b8cb840
Iurii Miedviediev <druidkuma@gmail.com>
2016-08-18 18:25:30 +0300

    Added migration script for base properties

---
+++ src/main/resources/db/migration/V14__insert_base_properties__.sql
@@ -0,0 +1,4 @@
+INSERT INTO property (pr_key, pr_country_id, pr_value, pr_last_modified) VALUES
+  ('permalinkGenerationStrategy', null, 'postName', now()),
+  ('autoDeleteDefault', null, 'false', now()),
+  ('autoCreateDefault', null, 'true', now());
