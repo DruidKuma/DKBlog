8d3575843f89fdb6b2f383f6b50165fc9d1cf5e1
Iurii Miedviediev <druidkuma@gmail.com>
2016-08-19 18:58:55 +0300

    Implemented stub for filling properties from yml files

--- src/main/java/com/druidkuma/blog/service/property/FilePropertiesHolder.java
+++ src/main/java/com/druidkuma/blog/service/property/FilePropertiesHolder.java
@@ -1,6 +1,13 @@
 package com.druidkuma.blog.service.property;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 /**
  * Created by Iurii Miedviediev
@@ -12,8 +19,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class FilePropertiesHolder {
 
+    @Value("#{'${properties.filenames}'.split(',')}")
+    private Resource[] locations;
+
+    private PropertySource propertySource;
+
     public String getProperty(String key) {
-        //TODO
-        return null;
+        return String.valueOf(propertySource.getProperty(key));
+    }
+
+    @PostConstruct
+    public void fillPropertiesFromFile() throws IOException {
+        for (Resource location : locations) {
+            propertySource = new YamlPropertySourceLoader().load("file", location, null);
+        }
     }
 }
