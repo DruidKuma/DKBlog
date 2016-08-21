d89abb5404d73fcd134c0e497874aa0a7c405dad
DruidKuma <druidkuma@gmail.com>
2016-08-22 00:54:25 +0300

    Created stub for i18n panel

--- src/main/resources/static/custom/i18nPanel/i18nPanelController.js
+++ src/main/resources/static/custom/i18nPanel/i18nPanelController.js
@@ -8,51 +8,6 @@ angular.module("blogApp")
             $scope.pageHeading.title = "I18N Panel";
         });
 
-        $scope.loadTopLevelGroups = function() {
-            I18NService.groupNames("").then(function(response) {
-                $scope.topLevelGroupNames = response.data;
-            });
-        };
+        $scope.chosenGroupParts = ['layout', 'sidebar', 'menu'];
 
-        $scope.loadGroup = function(group) {
-            //console.log(group);
-        };
-
-        $scope.topLevelGroups = [
-            {
-                name: 'layout',
-                translations: [
-                    {key: 'domain', valueFrom: 'Tyres', valueTo: 'Reifen'},
-                    {key: 'extension', valueFrom: 'NET', valueTo: 'DE'}
-                ],
-                children: [
-                    {
-                        name: 'menu',
-                        translations: [
-                            {key: 'dashboard', valueFrom: 'Dashboard', valueTo: 'Kugelschreiber'},
-                            {key: 'publishedPosts', valueFrom: 'Published Posts', valueTo: 'Schmeter Linger'}
-                        ],
-                        children: []
-                    },
-                    {
-                        name: 'titles',
-                        translations: [
-                            {key: 'listTitle', valueFrom: 'All Posts', valueTo: 'Alle Posten'},
-                            {key: 'i18nTitle', valueFrom: 'I18N Panel', valueTo: 'I18N Panelle'}
-                        ],
-                        children: []
-                    }
-                ]
-            },
-            {
-                name: 'postList',
-                translations: [
-                    {key: 'siska', valueFrom: 'Sosiska', valueTo: 'Sosiso4ka'},
-                    {key: 'piska', valueFrom: 'Pipiska', valueTo: 'Pipiso4ka'}
-                ],
-                children: []
-            }
-        ];
-
-        //$scope.loadTopLevelGroups();
     }]);
