bd4881c13a754d8602287ce40b4fbd30e12889ed
DruidKuma <druidkuma@gmail.com>
2016-08-22 10:00:33 +0300

    Styled and stubbed layout for the i18n panel

--- src/main/resources/static/custom/i18nPanel/i18nPanelController.js
+++ src/main/resources/static/custom/i18nPanel/i18nPanelController.js
@@ -7,7 +7,17 @@ angular.module("blogApp")
         $scope.$on('$routeChangeSuccess', function () {
             $scope.pageHeading.title = "I18N Panel";
         });
+        $scope.targetCountry = $scope.currentCountry;
+
 
         $scope.chosenGroupParts = ['layout', 'sidebar', 'menu'];
+
+        $scope.changeTargetCountry = function(country) {
+            $scope.targetCountry = country;
+        };
+
+        $scope.resetFilter = function() {
+            $scope.chosenGroupParts = [];
+        }
 
     }]);
