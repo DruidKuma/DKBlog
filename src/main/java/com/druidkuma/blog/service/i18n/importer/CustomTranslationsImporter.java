1d097020ced913bf1d16cfd7feb3bc3c952df77e
Iurii Miedviediev <druidkuma@gmail.com>
2016-09-01 15:13:04 +0300

    Implemented translations importer in custom format

--- src/main/java/com/druidkuma/blog/service/i18n/importer/CustomTranslationsImporter.java
+++ src/main/java/com/druidkuma/blog/service/i18n/importer/CustomTranslationsImporter.java
@@ -1,7 +1,18 @@
 package com.druidkuma.blog.service.i18n.importer;
 
+import com.druidkuma.blog.domain.i18n.TranslationGroup;
+import com.druidkuma.blog.util.Strings;
+import lombok.SneakyThrows;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Iurii Miedviediev
@@ -12,8 +23,31 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Component
 public class CustomTranslationsImporter extends TranslationsImporter {
+
     @Override
+    @SneakyThrows
     public void importTranslations(MultipartFile file, String columnSeparator, String rowSeparator) {
-        //TODO
+        if(Strings.isEmpty(columnSeparator)) columnSeparator = ",";
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), "UTF-8"));
+
+        List<String> header = readRow(reader, rowSeparator, columnSeparator);
+        List<String> languages = header.subList(1, header.size());
+
+        List<String> translationRow;
+        while (!(translationRow = readRow(reader, rowSeparator, columnSeparator)).isEmpty()) {
+            String key = translationRow.get(0);
+            List<String> langValues = translationRow.subList(1, translationRow.size());
+            for (String translation : langValues) {
+                String langIsoCode = languages.get(langValues.indexOf(translation));
+                TranslationGroup translationGroup = retrieveTranslationGroup(StringUtils.substringBeforeLast(key, "."));
+                createTranslation(StringUtils.substringAfterLast(key, "."), translation, langIsoCode, translationGroup);
+            }
+        }
+    }
+
+    private List<String> readRow(BufferedReader reader, String rowSeparator, String columnSeparator) throws IOException {
+        String line = reader.readLine();
+        return line == null ? Collections.emptyList() : Arrays.asList(Strings.substringBeforeLast(line, rowSeparator).split(columnSeparator));
     }
 }
